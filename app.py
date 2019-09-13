526f702aeaade705b16aa1ee18c1528096b813c8
RaviChandharpb <ravifabulous471@gmail.com>
2019-09-13 11:11:06 +0530

    python beginner code

---
+++ app.py
@@ -0,0 +1,41 @@
+print('Ravi Chandhar, I am a software developer in calibraint technologies')
+print('$' * 10);
+num1 = 10
+num2 = 12
+num3 = 13
+print(num1, num2, num3)
+
+
+# getting input from the use
+name = input('What is your name?')
+print('you have entered', name)
+
+# Type conversion
+birth_year = input('Birth year?')
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(age)
+
+# multi line
+course = ''' Hi, 
+ravi chandhare'''
+
+print(course)
+
+#string
+str1 = 'Python ooo'
+# from front
+print(str1[1])
+# from back
+print(str1[-2])
+print(str1[0:2])
+#full
+print(str1[0:])
+# formatted string
+msg = f'{str1}'
+print(msg)
+
+
+'adkada' in 'cascascaas'
+
+
